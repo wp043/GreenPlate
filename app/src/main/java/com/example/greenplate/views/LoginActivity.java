a3938f764ca97dcb011f86c010bab44cfe373464
HuangDerek <huangderek04@gmail.com>
2024-02-23 17:47:05 -0500

    Added views for login screen (LoginActivity.java and login_screen.xml).

---
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.greenplate.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenplate.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+}
