fe144217c174e5f358a59f4840f0b385d0f16615
NMX0710 <elvanie0710@gmail.com>
2024-02-23 17:25:20 -0500

    update splash

--- app/src/main/java/com/example/greenplate/views/SplashScreenActivity.java
+++ app/src/main/java/com/example/greenplate/views/SplashScreenActivity.java
@@ -1,12 +1,13 @@
 package com.example.greenplate.views;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.greenplate.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
