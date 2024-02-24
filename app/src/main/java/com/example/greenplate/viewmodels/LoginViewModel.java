924794955e443206ce362d0bdc664124f203d255
HuangDerek <huangderek04@gmail.com>
2024-02-24 09:48:59 -0500

    Add login functionality view model attempt

--- app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
@@ -1,18 +1,41 @@
 package com.example.greenplate.viewmodels;
 
-import com.example.greenplate.models.User;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
-public class LoginViewModel {
-    private static LoginViewModel instance;
-    final private User user;
+import androidx.annotation.NonNull;
+
+import com.example.greenplate.models.User;
+import com.example.greenplate.views.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
+public class LoginViewModel{
+    final private FirebaseAuth mAuth;
+    private boolean status;
+
 
     public LoginViewModel() {
-        user = new User();
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    // DO NOT MODIFY METHOD
-    public void setUser(String email, String password) {
-        user.setEmail(email);
-        user.setPassword(password);
+
+    public boolean checkUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    status = task.isSuccessful();
+                });
+                //.addOnSuccessListener(authResult -> status = true)
+                //.addOnFailureListener(e -> status = false);
+        return status;
     }
+
 }
