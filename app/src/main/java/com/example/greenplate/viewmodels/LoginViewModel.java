8af3a02654e9e4c96ac363e22eacfa5680af0d13
HuangDerek <huangderek04@gmail.com>
2024-02-23 20:48:01 -0500

    Update LoginViewModel.java with constructor and getInstance()

--- app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
@@ -1,4 +1,20 @@
 package com.example.greenplate.viewmodels;
 
+import com.example.greenplate.models.User;
+
 public class LoginViewModel {
+    private static LoginViewModel instance;
+    private User user;
+
+    public LoginViewModel() {
+        user = new User();
+    }
+
+    // DO NOT MODIFY METHOD
+    public static synchronized LoginViewModel getInstance() {
+        if (instance == null) {
+            instance = new LoginViewModel();
+        }
+        return instance;
+    }
 }
