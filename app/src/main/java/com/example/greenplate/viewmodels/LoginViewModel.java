b81a12d389fe0bd39bb3035607e75085f746aed5
HuangDerek <huangderek04@gmail.com>
2024-02-23 20:50:06 -0500

    Remove getInstance() from LoginViewModel.java

--- app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
@@ -11,10 +11,5 @@ public class LoginViewModel {
     }
 
     // DO NOT MODIFY METHOD
-    public static synchronized LoginViewModel getInstance() {
-        if (instance == null) {
-            instance = new LoginViewModel();
-        }
-        return instance;
-    }
+
 }
