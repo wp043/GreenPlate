5555ef05763a6f4c4eb5bbf6e960e1f1f7228a65
HuangDerek <huangderek04@gmail.com>
2024-02-23 21:03:36 -0500

    Add button logic for loginButton and signupButton

--- app/src/main/java/com/example/greenplate/views/LoginActivity.java
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.greenplate.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +13,59 @@ import com.example.greenplate.viewmodels.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel viewModel;
-    private EditText emailTextView;
-    private EditText passwordTextView;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
-        emailTextView = findViewById(R.id.loginEmail);
-        passwordTextView = findViewById(R.id.loginPassword);
+        emailEditText = findViewById(R.id.loginEmail);
+        passwordEditText = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        signupButton = findViewById(R.id.signupButton);
+        viewModel = new LoginViewModel();
+
+        loginButton.setOnClickListener(v -> {
+            // Check credentials
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if (email.isEmpty()) {
+                Toast.makeText(getApplicationContext(),
+                                "Please enter an email!",
+                                Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(getApplicationContext(),
+                                "Please enter an email!",
+                                Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            viewModel.setUser(email, password);
+            if (viewModel.checkPassword()) {
+                // Switch activity screen to main page
+            }
+
+            Toast.makeText(getApplicationContext(),
+                            "Incorrect email or password.",
+                            Toast.LENGTH_LONG)
+                    .show();
+        });
+
+        signupButton.setOnClickListener(v -> {
+            // Go to register page
+            Intent intent = new Intent(LoginActivity.this, AccountCreateActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
