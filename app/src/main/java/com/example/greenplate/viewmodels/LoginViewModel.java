a5ee56563e5150533813fb4635f837e42f430c57
HuangDerek <huangderek04@gmail.com>
2024-02-23 21:04:15 -0500

    Add setUser() method in LoginViewModel.java

--- app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
@@ -4,12 +4,15 @@ import com.example.greenplate.models.User;
 
 public class LoginViewModel {
     private static LoginViewModel instance;
-    private User user;
+    final private User user;
 
     public LoginViewModel() {
         user = new User();
     }
 
     // DO NOT MODIFY METHOD
-
+    public void setUser(String email, String password) {
+        user.setEmail(email);
+        user.setPassword(password);
+    }
 }
