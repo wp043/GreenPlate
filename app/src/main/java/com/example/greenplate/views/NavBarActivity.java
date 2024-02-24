c72e11c6a5fb32d9a247bafd020a8df726a31ee0
Steven Zhang <stevenzhang070302@gmail.com>
2024-02-23 20:04:01 -0500

    Updated new fragments for each of the navbar screen fragments we will be using.

--- app/src/main/java/com/example/greenplate/views/NavBarActivity.java
+++ app/src/main/java/com/example/greenplate/views/NavBarActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.greenplate.InputMealFragment;
 import com.example.greenplate.R;
 import com.example.greenplate.databinding.NavigationBarBinding;
 
@@ -28,9 +29,9 @@ public class NavBarActivity extends AppCompatActivity {
         navBind.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.inputMeals) {
-                // Handle input meals selection
+                replaceFragment(new InputMealFragment());
             } else if (itemId == R.id.recipes) {
-                // Handle recipes selection
+                replaceFragment(new RecipeFragment());
             } else if (itemId == R.id.ingredients) {
                 replaceFragment(new IngredientFragment());
             } else if (itemId == R.id.shoppingList) {
