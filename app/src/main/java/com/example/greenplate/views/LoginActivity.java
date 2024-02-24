871d3d4ab16775dc64f4b5a85fd85d754ff4edf5
HuangDerek <huangderek04@gmail.com>
2024-02-24 09:48:41 -0500

    Add login functionality attempt

--- app/src/main/java/com/example/greenplate/views/LoginActivity.java
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -6,10 +6,16 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
 import com.example.greenplate.viewmodels.LoginViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel viewModel;
@@ -17,10 +23,20 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button loginButton;
     private Button signupButton;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(getApplicationContext(),
+                            "User already logged in.",
+                            Toast.LENGTH_LONG)
+                    .show();
+            mAuth.signOut();
+        }
         setContentView(R.layout.login_screen);
 
         emailEditText = findViewById(R.id.loginEmail);
@@ -44,28 +60,57 @@ public class LoginActivity extends AppCompatActivity {
 
             if (password.isEmpty()) {
                 Toast.makeText(getApplicationContext(),
-                                "Please enter an email!",
+                                "Please enter a password!",
                                 Toast.LENGTH_LONG)
                         .show();
                 return;
             }
 
-            viewModel.setUser(email, password);
-            if (viewModel.checkPassword()) {
-                // Switch activity screen to main page
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(getApplicationContext(),
+                                                "Successful.",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(getApplicationContext(),
+                                                "Incorrect.",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    });
+
+
+            // Sign in failed
+            if (!viewModel.checkUser(email, password)) {
+                Toast.makeText(getApplicationContext(),
+                                "Incorrect email or password.",
+                                Toast.LENGTH_LONG)
+                        .show();
+                return;
             }
 
+
+            // Sign in successful
+            Intent intent = new Intent(LoginActivity.this, MainFragment.class);
+            startActivity(intent);
             Toast.makeText(getApplicationContext(),
-                            "Incorrect email or password.",
+                            "Sign in successful.",
                             Toast.LENGTH_LONG)
                     .show();
         });
+
 
         signupButton.setOnClickListener(v -> {
             // Go to register page
             Intent intent = new Intent(LoginActivity.this, AccountCreateActivity.class);
             startActivity(intent);
         });
-
     }
 }
