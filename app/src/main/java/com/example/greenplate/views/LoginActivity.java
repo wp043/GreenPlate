b2e6132559091ef1e81946553add8daf8a67c817
HuangDerek <huangderek04@gmail.com>
2024-02-25 04:22:36 -0500

    Removed excess imports and instance variables

--- app/src/main/java/com/example/greenplate/views/LoginActivity.java
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -4,18 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
 import com.example.greenplate.viewmodels.LoginViewModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel viewModel;
@@ -23,8 +17,6 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button loginButton;
     private Button signupButton;
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
