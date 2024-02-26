3b095477246cd96cabca731db8c04c2639e35605
Steven Zhang <stevenzhang070302@gmail.com>
2024-02-25 20:00:42 -0500

    Updating Create account functionality to transition directly to Navigation bar main page

--- app/src/main/java/com/example/greenplate/views/SplashScreenActivity.java
+++ app/src/main/java/com/example/greenplate/views/SplashScreenActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 //import android.os.Handler;
 //import androidx.appcompat.app.AppCompatActivity;
-//import com.example.greenplate.R;
+import com.example.greenplate.R;
 
 public class SplashScreenActivity extends Activity {
     @Override
