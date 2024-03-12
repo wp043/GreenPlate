d05140164d9b59603ccd7b3c0d1b653f3fda9496
Hannah Huang <hhy.huanghy@gmail.com>
2024-03-11 21:20:49 -0400

    updated personalactivity

--- app/src/main/java/com/example/greenplate/views/PersonalActivity.java
+++ app/src/main/java/com/example/greenplate/views/PersonalActivity.java
@@ -1,105 +1,3 @@
-//package com.example.greenplate.views;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.view.inputmethod.InputMethodManager;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.RadioButton;
-//import android.widget.RadioGroup;
-//import android.widget.TextView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.lifecycle.ViewModelProvider;
-//
-//import com.example.greenplate.R;
-//import com.example.greenplate.models.GreenPlateStatus;
-//import com.example.greenplate.models.Personal;
-//import com.example.greenplate.viewmodels.UserInfoViewModel;
-//
-//public class PersonalActivity extends AppCompatActivity {
-//    private UserInfoViewModel userInfoVM;
-//    private EditText heightField;
-//    private EditText weightField;
-//    private RadioGroup genderGroup;
-//    private Button submitButton;
-//    private Button backButton;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.personal_info);
-//
-//        userInfoVM = new ViewModelProvider(this).get(UserInfoViewModel.class);
-//
-//        TextView currheight = findViewById(R.id.currentHeight);
-//        TextView currweight = findViewById(R.id.currentWeight);
-//        TextView currgender = findViewById(R.id.currentGender);
-//        //display current info
-//        userInfoVM.getUserHeight(currheight).observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(String height) {
-//                currheight.setText(height);
-//            }
-//        });
-//        userInfoVM.getUserWeight(currweight);
-//        userInfoVM.getUserGender(currgender);
-//
-//        //update information as needed
-//        heightField = findViewById(R.id.editTextHeight);
-//        weightField = findViewById(R.id.editTextWeight);
-//        genderGroup = findViewById(R.id.radioGroupGender);
-//        submitButton = findViewById(R.id.buttonSubmit);
-//        backButton = findViewById(R.id.buttonBackToHome);
-//        submitButton.setOnClickListener(v -> {
-//            String height = heightField.getText().toString().trim();
-//            String weight = weightField.getText().toString().trim();
-//            int selectedGenderId = genderGroup.getCheckedRadioButtonId();
-//            RadioButton selectedGenderButton = findViewById(selectedGenderId);
-//            String gender = selectedGenderButton == null ? "" : selectedGenderButton.getText().toString();
-//
-//            if (!userInfoVM.isInputDataValid(height, weight, gender, heightField, weightField, selectedGenderButton)) {
-//                return;
-//            }
-//
-//            // Update the user info in ViewModel
-//            Personal person = new Personal(height, weight, gender);
-//            userInfoVM.editPersonalInformation(person);
-//
-//            Intent intent = new Intent(PersonalActivity.this, HomeFragment.class);
-//            startActivity(intent);
-//        });
-//
-//        backButton.setOnClickListener(v -> {
-//            // Go to create account page
-//            Intent intent = new Intent(PersonalActivity.this, HomeFragment.class);
-//            startActivity(intent);
-//        });
-//    }
-//
-//    private void setSelectedGenderRadioButton(String gender) {
-//        switch (gender) {
-//            case "Male":
-//                ((RadioButton)findViewById(R.id.radioButtonMale)).setChecked(true);
-//                break;
-//            case "Female":
-//                ((RadioButton)findViewById(R.id.radioButtonFemale)).setChecked(true);
-//                break;
-//            default:
-//                break;
-//        }
-//    }
-//
-//
-//    // Method for closing the keyboard in fragment.
-//    private void hideKeyboardFrom(Context context, View view) {
-//        InputMethodManager imm = (InputMethodManager)
-//                context.getSystemService(Context.INPUT_METHOD_SERVICE);
-//        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-//    }
-//}
 package com.example.greenplate.views;
 
 import android.content.Intent;
@@ -107,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -124,10 +23,14 @@ public class PersonalActivity extends AppCompatActivity {
     private Button submitButton;
     private Button backButton;
 
+    private TextView displayHeightField;
+    private TextView displayWeightField;
+    private TextView displayGenderField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_info);
+        setContentView(R.layout.personal_info); //connect to XML file (personal_info.xml)
 
         userInfoVM = new ViewModelProvider(this).get(UserInfoViewModel.class);
         heightField = findViewById(R.id.editTextHeight);
@@ -136,12 +39,17 @@ public class PersonalActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.buttonSubmit);
         backButton = findViewById(R.id.buttonBackToHome);
 
+        displayHeightField=findViewById(R.id.currentHeight);
+        displayWeightField=findViewById(R.id.currentWeight);
+        displayGenderField=findViewById(R.id.currentGender);
+
         // Observe the LiveData from the ViewModel
+        //update the personal displayed gender, height, weight if there's any change in the database
         userInfoVM.getUserPersonalInfo().observe(this, personal -> {
             if (personal != null) {
-                heightField.setText(personal.getHeight());
-                weightField.setText(personal.getWeight());
-                setSelectedGenderRadioButton(personal.getGender());
+                displayHeightField.setText(personal.getHeight());
+                displayWeightField.setText(personal.getWeight());
+                displayGenderField.setText(personal.getGender());
             }
         });
 
@@ -163,12 +71,12 @@ public class PersonalActivity extends AppCompatActivity {
         backButton.setOnClickListener(v -> finish());
     }
 
-    private void setSelectedGenderRadioButton(String gender) {
-        if (gender.equals("Male")) {
-            ((RadioButton)findViewById(R.id.radioButtonMale)).setChecked(true);
-        } else if (gender.equals("Female")) {
-            ((RadioButton)findViewById(R.id.radioButtonFemale)).setChecked(true);
-        }
-        // Add more conditions if there are more genders
-    }
+//    private void setSelectedGenderRadioButton(String gender) {
+//        if (gender.equals("Male")) {
+//            ((RadioButton)findViewById(R.id.radioButtonMale)).setChecked(true);
+//        } else if (gender.equals("Female")) {
+//            ((RadioButton)findViewById(R.id.radioButtonFemale)).setChecked(true);
+//        }
+//        // Add more conditions if there are more genders
+//    }
 }
