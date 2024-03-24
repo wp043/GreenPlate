6de58785bfcd775de7c5c415e86debf0cabeec42
HuangDerek <huangderek04@gmail.com>
2024-03-24 16:16:30 -0400

    Created CookbookManager class.

---
+++ app/src/main/java/com/example/greenplate/viewmodels/CookbookManager.java
@@ -0,0 +1,163 @@
+package com.example.greenplate.viewmodels;
+
+import android.util.Log;
+
+import com.example.greenplate.models.GreenPlateStatus;
+import com.example.greenplate.models.Ingredient;
+import com.example.greenplate.models.Recipe;
+import com.example.greenplate.models.RetrievableItem;
+import com.example.greenplate.viewmodels.listeners.OnDataRetrievedCallback;
+import com.example.greenplate.viewmodels.listeners.OnDuplicateCheckListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CookbookManager implements Manager {
+    private FirebaseDatabase database;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private FirebaseUser currentUser;
+
+
+    /**
+     * Constructor for PantryManager.
+     */
+    public CookbookManager() {
+        try {
+            database = FirebaseDatabase.getInstance();
+            mAuth = FirebaseAuth.getInstance();
+            currentUser = mAuth.getCurrentUser();
+            if (currentUser == null) {
+                throw new RuntimeException("PantryManager: There's no user signed in.");
+            }
+            myRef = database.getReference("user").child(currentUser.getUid())
+                    .child("pantries");
+        } catch (Exception e) {
+            Log.d("Issue", "PantryManager: " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void retrieve(OnDataRetrievedCallback callback) {
+        List<RetrievableItem> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                    try {
+                        // Query name of recipe from database
+                        String name = childSnapshot.child("name")
+                                .getValue(String.class);
+
+                        // Query ingredients of recipe from database
+                        Map<Ingredient, Integer> ingredients = new HashMap<>();
+                        DataSnapshot ingredientsSnapshot = childSnapshot.child("ingredients");
+                        for (DataSnapshot ingredientSnapshot: ingredientsSnapshot.getChildren()) {
+                            String ingredientName = ingredientSnapshot.child("name")
+                                    .getValue(String.class);
+                            int quantity = ingredientSnapshot.child("quantity")
+                                    .getValue(Integer.class);
+                            ingredients.put(new Ingredient(ingredientName), quantity);
+                        }
+
+                        // Query instructions of recipe from database
+                        List<String> instructions = new ArrayList<>();
+                        DataSnapshot instructionsSnapshot = childSnapshot.child("instructions");
+                        for (DataSnapshot instructionSnapshot: instructionsSnapshot.getChildren()) {
+                            String instruction = instructionSnapshot.getValue(String.class);
+                            instructions.add(instruction);
+                        }
+
+                        Recipe recipe = new Recipe(name, ingredients, instructions);
+                        items.add(recipe);
+                    } catch (Exception e) {
+                        Log.d("Issue", "CookbookManager failed to read from db for "
+                                + e.getLocalizedMessage());
+                    }
+                }
+                callback.onDataRetrieved(items);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.d("Issue", "CookbookManager: error in querying the db.");
+                callback.onDataRetrieved(null);
+            }
+        });
+    }
+
+    /**
+     * Add a recipe to the database.
+     * @param recipe - the recipe to add
+     * @return the status of the operation
+     */
+    public GreenPlateStatus addRecipe(Recipe recipe) {
+        try {
+            DatabaseReference cookbookRef = myRef.child("Cookbook");
+            String recipeKey = cookbookRef.push().getKey();
+            if (recipeKey == null) {
+                throw new RuntimeException("Failed to generate recipe key");
+            }
+            // Add name of recipe to database
+            cookbookRef.child(recipeKey).child("name").setValue(recipe.getName());
+
+            // Add ingredients of recipe to database
+            DatabaseReference ingredientsRef = cookbookRef.child(recipeKey).child("ingredients");
+            for (Map.Entry<Ingredient, Integer> ingredient: recipe.getIngredients().entrySet()) {
+                String ingredientKey = ingredientsRef.push().getKey();
+                if (ingredientKey == null) {
+                    throw new RuntimeException("Failed to generate ingredient key");
+                }
+                ingredientsRef.child(ingredientKey).child("name")
+                        .setValue(ingredient.getKey().getName());
+                ingredientsRef.child(ingredientKey).child("quantity")
+                        .setValue((int) ingredient.getValue());
+            }
+
+            DatabaseReference instructionsRef = cookbookRef.child(recipeKey).child("instructions");
+            for (String instruction: recipe.getInstructions()) {
+                String instructionKey = instructionsRef.push().getKey();
+                if (instructionKey == null) {
+                    throw new RuntimeException("Failed to generate instruction key");
+                }
+                instructionsRef.child(instructionKey).setValue(instruction);
+            }
+
+        } catch (Exception e) {
+            Log.d("Failure", "PantryManager failure due to: " + e.getLocalizedMessage());
+            return new GreenPlateStatus(false, "Add meal: " + e.getLocalizedMessage());
+        }
+        return new GreenPlateStatus(true,
+                String.format("%s added to database successfully", recipe));
+    }
+
+    /**
+     * Check whether the input recipe is duplicate.
+     * @param recipe - the recipe to check
+     * @param listener - the listener to update
+     */
+    public void isRecipeDuplicate(Recipe recipe, OnDuplicateCheckListener listener) {
+        retrieve(items -> {
+            boolean isDuplicate = false;
+            for (RetrievableItem item : items) {
+                if (item.equals(recipe)) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            listener.onDuplicateCheckCompleted(isDuplicate);
+        });
+    }
+}
