4f50063630e3babd0cfdbec1860f45f604ff111e
Steven Zhang <stevenzhang070302@gmail.com>
2024-04-17 16:26:19 -0400

    Added Advanced Class Structure and Method Headers according to Observer Pattern for CalorieLeftDisplay and MealBreakdownDisplay observers for chart functionality

---
+++ app/src/main/java/com/example/greenplate/viewmodels/listeners/OnChartUpdateListenerObserver.java
@@ -0,0 +1,5 @@
+package com.example.greenplate.viewmodels.listeners;
+// Observer Pattern
+public interface OnChartUpdateListenerObserver {
+    public void onChartUpdate(); // TO-DO: Add in necessary parameters in here
+}
