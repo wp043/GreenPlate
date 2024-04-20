b4d39773e767e91ec8bd7bd8df5f8c53ab9808c9
wp043 <22.wendy.pan@gmail.com>
2024-04-20 15:15:30 -0400

    subtract ingredient amount after cooking

--- app/src/androidTest/java/com/example/greenplate/CookTests.java
+++ app/src/androidTest/java/com/example/greenplate/CookTests.java
@@ -2,13 +2,20 @@ package com.example.greenplate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import androidx.annotation.NonNull;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.greenplate.models.GreenPlateStatus;
+import com.example.greenplate.models.Ingredient;
 import com.example.greenplate.models.Personal;
+import com.example.greenplate.models.Recipe;
+import com.example.greenplate.viewmodels.IngredientViewModel;
+import com.example.greenplate.viewmodels.RecipeViewModel;
 import com.example.greenplate.viewmodels.UserInfoViewModel;
+import com.example.greenplate.viewmodels.helpers.AvailabilityReportGenerator;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -16,11 +23,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import junit.framework.TestCase;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 @RunWith(AndroidJUnit4.class)
@@ -66,5 +79,31 @@ public class CookTests {
                 });
     }
 
+    @Test
+    public void subtractIngredient() {
+        RecipeViewModel recipeViewModel = new RecipeViewModel();
+        assertNotNull(recipeViewModel);
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Tomato", 100, 2, null));
+        List<String> instructions = new ArrayList<>();
+        Recipe recipe = new Recipe("Tomato Salad", ingredients, instructions);
+        recipeViewModel.addRecipe(recipe, success -> {
+            assertTrue("Recipe added successfully!", success);
+        });
 
+        // set multiplicity changes the amount in pantry database
+        ingredients.get(0).setMultiplicity(3);
+        IngredientViewModel ingredientViewModel = new IngredientViewModel();
+        ingredientViewModel.addIngredient(ingredients.get(0), (success, message) -> {
+            Assert.assertTrue(success);
+        });
+
+        AvailabilityReportGenerator availabilityReportGenerator
+                = AvailabilityReportGenerator.getInstance();
+        availabilityReportGenerator.getAvailable(report -> {
+            Map<Ingredient, Double> available = report.get("Tomato Salad");
+            TestCase.assertEquals(1, available.size());
+            TestCase.assertEquals(1.0, available.get("Tomato"));
+        });
+    }
 }
